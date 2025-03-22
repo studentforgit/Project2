94ac2bd0206c254c635fa158a52a6287fa691643
studentforgit <21f3001993@ds.study.iitm.ac.in>
2025-03-22 08:58:35 +0530

    upto q5

--- main.py
+++ main.py
@@ -1,14 +1,11 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import os
-import zipfile
-import pandas as pd
-import PyPDF2
-from werkzeug.utils import secure_filename
-import json
 from file_handler import process_uploaded_file
 from question_handler import process_question
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/')
 def home():
@@ -16,10 +13,11 @@ def home():
 
 
 
-
-
-@app.route('/api/', methods=['POST'])
+@app.route('/api/', methods=['POST', 'OPTIONS'])
 def answer_question():
+    if request.method == 'OPTIONS':
+        return _build_cors_preflight_response()
+    
     question = request.form.get('question')
     file = request.files.get('file')
     
@@ -29,9 +27,19 @@ def answer_question():
     extracted_data = process_uploaded_file(file) if file else None
     answer = process_question(question, extracted_data)
     
-    return jsonify({'answer': str(answer)})
+    return _corsify_actual_response(jsonify({'answer': str(answer)}))
+
+# Helper functions to handle CORS responses
+def _build_cors_preflight_response():
+    response = jsonify({})
+    response.headers.add("Access-Control-Allow-Origin", "*")
+    response.headers.add("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+    response.headers.add("Access-Control-Allow-Headers", "Content-Type, Authorization")
+    return response
+
+def _corsify_actual_response(response):
+    response.headers.add("Access-Control-Allow-Origin", "*")
+    return response
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-
